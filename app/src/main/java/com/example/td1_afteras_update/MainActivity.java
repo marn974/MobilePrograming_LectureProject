711ad9b93e00a4203de87978d224c6dd3ada989b
marn974 <62256938+marn974@users.noreply.github.com>
2020-05-06 17:15:04 +0200

    Ajout de code

--- app/src/main/java/com/example/td1_afteras_update/MainActivity.java
+++ app/src/main/java/com/example/td1_afteras_update/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Je rajoute du code 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
